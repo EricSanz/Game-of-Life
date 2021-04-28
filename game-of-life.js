39cc747208e31fff50a13f226d59c32d1c1c596f
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-04-28 20:37:55 +0200

    created function for randomize cells alive

--- game-of-life.js
+++ game-of-life.js
@@ -6,4 +6,32 @@ const size = width + height;
 const scale = 6;
 const resolution = Math.round(size / scale);
 
+function settingUp() {
+    canvas.width = width;
+    canvas.height = height;
+    context.scale(scale, scale);
+    cells = createCells();
+    cellsAlive();
+};
+
+function createCells() {
+    let rows = new Array(resolution);
+    for (let x = 0; x < resolution; x++) {
+        let cols = new Array(resolution);
+        for (let y = 0; y < resolution; y++) {
+            cols[y] = false;
+        };
+        rows[x] = cols;
+    };
+
+    return rows;
+};
+
+function cellsAlive() {
+    for (let y = 0; y < resolution; y++) {
+        for (let x = 0; x < resolution; x++) {
+            Math.random() > 0.5 ? cells[x][y] = true : cells[x][y] = false;
+        }
+    }
+}
 
