a574669e53ecd9fe55180079fb864accf09c3d33
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-04-28 13:33:47 +0200

    creating canvas

---
+++ game-of-life.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const width = 1300;
+const height = 600;
+const size = width + height;
+const scale = 6;
+const resolution = Math.round(size / scale);
+
+
